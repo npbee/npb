b14e597145a544a7380f977418820e21d4d5e6a7
Nick Ball <nicholaspball@gmail.com>
2014-07-14 09:33:12 -0500

    added basic rdoc readme

---
+++ doc/app/js/search_index.js
@@ -0,0 +1,1 @@
+var search_data = {"index":{"searchIndex":["applicationcontroller","applicationhelper","readme"],"longSearchIndex":["applicationcontroller","applicationhelper",""],"info":[["ApplicationController","","ApplicationController.html","",""],["ApplicationHelper","","ApplicationHelper.html","",""],["README","","README_rdoc.html","","<p>README\n<p>Dependencies\n<p>Vagrant\n"]]}}
