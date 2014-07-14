74887ca822ef7e8fdef7c51e8b2b223228211a5b
Nick Ball <nicholaspball@gmail.com>
2014-07-14 18:58:15 -0500

    add content to static pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -6,7 +6,20 @@ describe "StaticPages" do
     before { visit root_path }
 
     it "Has the correct content" do
+
+      # Is the logo on the page?
       expect(page).to have_css('.site-logo')
+
+      # Check to make sure we have the right links on the page
     end
+  end
+
+  describe "#connect" do
+    before { visit '/connect' }
+
+    it "Has the correct content" do
+      expect(page).to have_content('Email Me')
+    end
+
   end
 end
