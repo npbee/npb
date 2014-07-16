368692ce44b69286b68d55f9b16060c4d481e167
Nick Ball <nicholaspball@gmail.com>
2014-07-16 18:52:12 -0500

    render admin layout on user controller, update connect to use accepting_projects attribute for button class

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
   before_filter :require_login
+  layout 'admin'
 
   # GET /users
   def index
