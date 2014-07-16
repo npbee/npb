fb6e0b5081874445b0fc0bc2759160f9bf88c385
Nick Ball <nicholaspball@gmail.com>
2014-07-16 17:00:53 -0500

    add tests for user pages authorization

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
-  #skip_before_filter :require_login, only: [:index, :new, :create]
+  before_filter :require_login
 
   # GET /users
   def index
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,5 +1,30 @@
 require 'rails_helper'
+require 'support/requests_spec_helper'
 
 RSpec.describe "Users", :type => :request do
+  include RequestsSpecHelper
+  
+  describe "User pages" do
+    before do
+      @user = FactoryGirl.create(:user)
+    end
 
+    context "when not logged in" do
+      before { visit '/users/' }
+      it "should redirect to the login page" do
+        expect(page.current_path).to eq('/login')
+      end
+    end
+
+    context "when logged in" do
+      before do
+        login_user('test@test.com', 'foobar85')
+        visit '/users'
+      end
+
+      it 'should allow access to the page' do
+        expect(page.current_path).to eq('/users')
+      end
+    end
+  end
 end
