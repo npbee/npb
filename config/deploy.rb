29eb95f94ca7b80cf1c20810092b4e3abd36e924
Nick Ball <nicholaspball@gmail.com>
2014-07-22 16:10:42 -0500

    update deploy to include font symlink

--- config/deploy.rb
+++ config/deploy.rb
@@ -21,7 +21,7 @@ set :keep_releases, 2
 set :linked_files, %w{config/database.yml config/secrets.yml config/seeds.rb}
 
 # dirs we want symlinking to shared
-set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/assets/fonts/193880}
 
 # what specs should be run before deployment is allowed to
 # continue, see lib/capistrano/tasks/run_tests.cap
