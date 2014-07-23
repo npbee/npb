ef290c11f25c3cd76de885b4aa2ee5e243c234c3
Nick Ball <nicholaspball@gmail.com>
2014-07-23 09:08:05 -0500

    update deploy script

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -4,7 +4,7 @@ set :branch, "master"
 # This is used in the Nginx VirtualHost to specify which domains
 # the app should appear on. If you don't yet have DNS setup, you'll
 # need to create entries in your local Hosts file for testing.
-set :server_name, "www.kinmill.com kinmill.com"
+set :server_name, "www.npbee.me npbee.me"
 
 # used in case we're deploying multiple versions of the same
 # app side by side. Also provides quick sanity checks when looking
