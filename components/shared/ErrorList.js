207c982b9ab5f557649d83111548f73dd065aa59
Nick Ball <nicholaspball@gmail.com>
2015-05-25 16:50:30 -0700

    build for deployment

--- components/shared/ErrorList.js
+++ components/shared/ErrorList.js
@@ -7,15 +7,15 @@ module.exports = React.createClass({
     },
 
     componentDidMount: function() {
-        
+
     },
 
     render: function() {
         return <div className="error-list">
             <ul>
             {this.props.errors.map(function(err, i) {
-                return <li 
-                    className="alert alert--error" 
+                return <li
+                    className="alert alert--error"
                     key={i}>
                     <img src="/static/images/icons/icomoon/user.svg" /><a>{err.error}</a></li>
             })}
