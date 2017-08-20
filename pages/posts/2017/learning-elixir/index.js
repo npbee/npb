9217e7f553c5c6c29dd7d19d58c9071947dd61b5
Nick Ball <nicholaspball@gmail.com>
2017-08-20 08:19:45 -0700

    Update date

--- pages/posts/2017/learning-elixir/index.js
+++ pages/posts/2017/learning-elixir/index.js
@@ -2,7 +2,7 @@ import Post from '../../../../layouts/post';
 import content from './index.json';
 
 export default function() {
-    return <Post date='August 19th, 2017'>
+    return <Post date='August 20th, 2017'>
         {content.content}
     </Post>;
 }
