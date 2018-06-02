fdcd0e5b522d39c5dda04c92e58e21c3caa331a4
Nick Ball <nicholaspball@gmail.com>
2018-06-02 10:04:22 -0700

    Optimize image, remove analytics

--- components/layout/main.js
+++ components/layout/main.js
@@ -53,19 +53,6 @@ export default withStyle(({ children, post }) => {
         />
         <title>Nick Ball</title>
         <meta name="viewport" content="width=device-width" />
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-                (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-                (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-                m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-                })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
-                ga('create', 'UA-46265154-1', 'auto');
-                ga('send', 'pageview');
-                `,
-          }}
-        />
       </Head>
 
       <Avatar />
