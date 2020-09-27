69410bdca4a95de11820e4ca22b0a4d8332415e9
Nick Ball <nicholaspball@gmail.com>
2020-09-27 12:12:57 -0700

    move to centered layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,7 @@ module.exports = {
       sm: "640px",
       // => @media (min-width: 640px) { ... }
 
-      md: "768px",
+      md: "700px",
       // => @media (min-width: 768px) { ... }
     },
   },
