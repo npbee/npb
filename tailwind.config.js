22673e8553557f1ca305920a20c7f67d839eb8e8
Nick Ball <nick@npbee.me>
2022-04-09 14:33:26 -0700

    Upgrade tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,27 +2,12 @@ const colors = require("tailwindcss/colors");
 
 // tailwind.config.js
 module.exports = {
-  purge: {
-    content: ["./src/site/**/*.njk", ".eleventy.js"],
-    safelistPatterns: [/h1/, /post/, /pre/, /code/, /language/],
-  },
-  variants: {
-    extend: {
-      textColor: ["active"],
-    },
-  },
+  content: ["./src/site/**/*.njk", ".eleventy.js"],
   theme: {
     extend: {
       colors: {
         primary: colors.pink,
       },
-    },
-    screens: {
-      sm: "640px",
-      // => @media (min-width: 640px) { ... }
-
-      md: "700px",
-      // => @media (min-width: 768px) { ... }
     },
   },
 };
