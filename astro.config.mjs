3091762cc4293a08b651b0e8507ab5f07978aae5
Nick Ball <nick@npbee.me>
2023-08-31 05:22:26 -0700

    Upgrade to Astro 3

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-import vercel from "@astrojs/vercel/edge";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  adapter: vercel({ analytics: true }),
+  adapter: vercel({ analytics: true, edgeMiddleware: true }),
   integrations: [
     tailwind({
       applyBaseStyles: false,
