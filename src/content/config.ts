24e64b607b1b1b7158dfd6ec1f967fe559d698ea
Nick Ball <nick@npbee.me>
2025-01-10 15:55:24 -0800

    Make Forms demo a draft

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,27 +1,28 @@
 import { defineCollection, z, CollectionEntry } from "astro:content";
 
 export const blogCollection = defineCollection({
-  type: 'content',
+  type: "content",
   schema: z.object({
     title: z.string(),
     date: z.date(),
     description: z.string(),
-    ogImage: z.string().optional()
+    ogImage: z.string().optional(),
   }),
 });
 
-export type Post = CollectionEntry<'blog'>
+export type Post = CollectionEntry<"blog">;
 
 const projectsCollection = defineCollection({
-  type: 'content',
+  type: "content",
   schema: z.object({
     title: z.string(),
     graphic: z.string(),
     link: z.string().url(),
+    draft: z.boolean().optional().default(false),
   }),
 });
 
-export type Project = CollectionEntry<'projects'>
+export type Project = CollectionEntry<"projects">;
 
 export const collections = {
   blog: blogCollection,
--- src/content/projects/helloworks-demo.md
+++ src/content/projects/helloworks-demo.md
@@ -2,6 +2,7 @@
 title: Dropbox Forms Demo
 graphic: helloworks.png
 link: https://www.hellosign.com/products/dropbox-forms#demo
+draft: true
 ---
 
 An interactive demo page for the product I built at Dropbox, Dropbox Forms. Built with [Preact](https://preactjs.com/).
